3b7cda01415cc8a80672319af2bf50cc3c184c97
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2021-06-28 14:08:01 +0900

    add: recoil selector examples

--- components/Counter/Counter.tsx
+++ components/Counter/Counter.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { countState } from '../../store/count';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import {
+  countOppositeState,
+  countState,
+  oppositeCountState,
+} from '../../store/count';
 
 interface CounterProps {}
 
 const Counter: React.FC<CounterProps> = ({}) => {
-  const [count, setCount] = useRecoilState(countState);
+  const setCount = useSetRecoilState(countState);
+  const count = useRecoilValue(oppositeCountState);
+  const [isOpposite, setIsOpposite] = useRecoilState(countOppositeState);
+
   return (
     <div>
       <div>Current Count: {count}</div>
       <div>
         <button onClick={() => setCount(count + 1)}>increment</button>
         <button onClick={() => setCount(count - 1)}>decrement</button>
+      </div>
+      <div>
+        <button onClick={() => setIsOpposite(!isOpposite)}>
+          show opposite count
+        </button>
       </div>
     </div>
   );
--- store/count.ts
+++ store/count.ts
@@ -1,6 +1,21 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const countState = atom({
   key: 'count',
   default: 0,
+});
+
+export const countOppositeState = atom({
+  key: 'countOppositeState',
+  default: false,
+});
+
+export const oppositeCountState = selector({
+  key: 'oppositeCountState',
+  get: ({ get }) => {
+    const count = get(countState);
+    const isOpposite = get(countOppositeState);
+
+    return count * (isOpposite ? -1 : 1);
+  },
 });
